8145657953e5bc7a732d882e23c9137bb9dd543b
chungphb <phamhuubaochung@gmail.com>
2021-04-06 23:15:31 +0700

    Lesson 31: Scrolling Backgrounds

--- src/test/TestScrolling.cpp
+++ src/test/TestScrolling.cpp
@@ -10,6 +10,8 @@ const int LEVEL_HEIGHT = 720;
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 
+namespace test_scrolling {
+
 struct Dot {
 public:
 	static constexpr int DOT_WIDTH = 20;
@@ -130,8 +132,8 @@ public:
 
 	bool loadMedia() override {
 		bool success = true;
-		if (!dotTexture.loadFromFile(renderer, "image/dot.png")) {
-			printf("Failed to load \"dot\" texture image!\n");
+		if (!dotTexture.loadFromFile(renderer, "image/red_dot.png")) {
+			printf("Failed to load \"red_dot\" texture image!\n");
 			success = false;
 		}
 		if (!backgroundTexture.loadFromFile(renderer, "image/background2.png")) {
@@ -196,8 +198,191 @@ private:
 	Texture backgroundTexture;
 };
 
+}
+
+namespace test_scrolling_backgrounds {
+
+struct Dot {
+public:
+	static constexpr int DOT_WIDTH = 20;
+	static constexpr int DOT_HEIGHT = 20;
+	static constexpr int DOT_VEL = 10;
+
+	Dot() {
+		posX = 0;
+		posY = 0;
+		velX = 0;
+		velY = 0;
+	}
+
+	void handleEvent(SDL_Event& e) {
+		if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
+			switch (e.key.keysym.sym) {
+				case SDLK_UP: {
+					velY -= DOT_VEL; break;
+				}
+				case SDLK_DOWN: {
+					velY += DOT_VEL; break;
+				}
+				case SDLK_LEFT: {
+					velX -= DOT_VEL; break;
+				}
+				case SDLK_RIGHT: {
+					velX += DOT_VEL; break;
+				}
+				default: {
+					break;
+				}
+			}
+		}
+		else if (e.type == SDL_KEYUP && e.key.repeat == 0) {
+			switch (e.key.keysym.sym) {
+				case SDLK_UP: {
+					velY += DOT_VEL; break;
+				}
+				case SDLK_DOWN: {
+					velY -= DOT_VEL; break;
+				}
+				case SDLK_LEFT: {
+					velX += DOT_VEL; break;
+				}
+				case SDLK_RIGHT: {
+					velX -= DOT_VEL; break;
+				}
+				default: {
+					break;
+				}
+			}
+		}
+	}
+
+	void move() {
+		posX += velX;
+		if (posX < 0 || posX + DOT_WIDTH > LEVEL_WIDTH) {
+			posX -= velX;
+		}
+		posY += velY;
+		if (posY < 0 || posY + DOT_HEIGHT > LEVEL_HEIGHT) {
+			posY -= velY;
+		}
+	}
+
+	void render(SDL_Renderer* renderer, Texture* dotTexture) {
+		dotTexture->render(renderer, posX, posY);
+	}
+
+	int getPosX() {
+		return posX;
+	}
+
+	int getPosY() {
+		return posY;
+	}
+
+private:
+	int posX, posY;
+	int velX, velY;
+};
+
+struct TestScrollingBackgrounds : public Window {
+public:
+	bool init() override {
+		bool success = true;
+		if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+			printf("SDL could not initialize! Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else {
+			if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+				printf("Warning: Linear texture filtering is not enabled!");
+			}
+			window = SDL_CreateWindow("Test motion!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+			if (!window) {
+				printf("Window could not be created! Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else {
+				renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+				if (!renderer) {
+					printf("Renderer could not be created! Error: %s\n", SDL_GetError());
+					success = false;
+				}
+				else {
+					SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+					int imgFlags = IMG_INIT_PNG;
+					if (!(IMG_Init(imgFlags) & imgFlags)) {
+						printf("SDL2_image could not initialize! Error: %s\n", IMG_GetError());
+						success = false;
+					}
+				}
+			}
+		}
+		return success;
+	}
+
+	bool loadMedia() override {
+		bool success = true;
+		if (!dotTexture.loadFromFile(renderer, "image/red_dot.png")) {
+			printf("Failed to load \"red_dot\" texture image!\n");
+			success = false;
+		}
+		if (!backgroundTexture.loadFromFile(renderer, "image/background3.png")) {
+			printf("Failed to load \"background3\" texture image!\n");
+			success = false;
+		}
+		return success;
+	}
+
+	void run() override {
+		bool quit = false;
+		SDL_Event e;
+		Dot dot;
+		int scrollingOffset = 0;
+		while (!quit) {
+			while (SDL_PollEvent(&e) != 0) {
+				if (e.type == SDL_QUIT) {
+					quit = true;
+				}
+				else {
+					dot.handleEvent(e);
+				}
+			}
+			dot.move();
+			scrollingOffset--;
+			if (scrollingOffset < -WINDOW_WIDTH) {
+				scrollingOffset = 0;
+			}
+			SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+			SDL_RenderClear(renderer);
+			SDL_SetRenderDrawColor(renderer, 0xC0, 0x00, 0x00, 0xFF);
+			backgroundTexture.render(renderer, scrollingOffset, 0);
+			backgroundTexture.render(renderer, scrollingOffset + WINDOW_WIDTH, 0);
+			dot.render(renderer, &dotTexture);
+			SDL_RenderPresent(renderer);
+		}
+	}
+
+	void close() override {
+		dotTexture.free();
+		backgroundTexture.free();
+		SDL_DestroyRenderer(renderer);
+		renderer = nullptr;
+		SDL_DestroyWindow(window);
+		window = nullptr;
+		IMG_Quit();
+		SDL_Quit();
+	}
+
+private:
+	SDL_Renderer* renderer = nullptr;
+	Texture dotTexture;
+	Texture backgroundTexture;
+};
+
+}
+
 int main(int argc, char** argv) {
-	TestScrolling mainWindow;
+	test_scrolling_backgrounds::TestScrollingBackgrounds mainWindow;
 	if (!mainWindow.init()) {
 		printf("Failed to initialize!\n");
 	} else {
